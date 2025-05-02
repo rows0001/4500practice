3f1067e7662df5975f3f545134418c51b3793208
Rowshni Fatema <rows0001@student.monash.edu>
2025-05-03 01:07:06 +1000

    fixing targets.R for model summary

--- _targets.R
+++ _targets.R
@@ -1,5 +1,6 @@
 library(targets)
 library(tarchetypes)
+library(purrr)
 
 source("R/functions.R")
 
@@ -10,11 +11,11 @@ list(
   tar_target(clean_data, clean_and_wrangle(raw_data)),
   tar_target(plot1, make_plot1(clean_data)),
   tar_target(plot2, make_plot2(clean_data)),
-  tar_target(model_fit, fit_model(clean_data)),  # returns nested df with models
+  tar_target(model_fit, fit_model(clean_data)),  # Fit the model, returns nested df with models
   tar_target(model_summary,
              model_fit %>%
-               mutate(tidy_model = map(model, broom::tidy)) %>%
-               select(country, tidy_model) %>%
-               unnest(tidy_model)
+               mutate(tidy_model = map(model, broom::tidy)) %>%  # Apply broom::tidy to each model
+               select(country, tidy_model) %>%  # Select country and the tidy model
+               unnest(tidy_model)  # Unnest the tidy model data
   )
 )
