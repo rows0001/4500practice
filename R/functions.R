6f2d099376fa42435b82c2b5c48d7b6278d3a1d3
Rowshni Fatema <rows0001@student.monash.edu>
2025-05-03 01:01:14 +1000

    fixing codes in analysis

--- R/functions.R
+++ R/functions.R
@@ -1,20 +1,48 @@
 
-clean_and_wrangle <- function(data) {
-  data %>%
-    filter(!is.na(some_column)) %>%
-    mutate(new_var = log(some_numeric_column + 1))
+clean_and_wrangle <- function(df) {
+  df |>
+    rename(
+      country = Entity,
+      life_satisfaction = `Cantril ladder score`,
+      gdp_per_capita = `GDP per capita, PPP (constant 2021 international $)`
+    ) |>
+    select(country, Year, life_satisfaction, gdp_per_capita) |>
+    filter(country %in% c(
+      "Canada", "France", "Germany", "Italy", "Japan",
+      "Russia", "United States", "United Kingdom", "Australia"
+    )) |>
+    drop_na()
 }
 
-make_plot1 <- function(data) {
-  ggplot(data, aes(x = new_var)) +
-    geom_histogram()
+make_plot1 <- function(df) {
+  ggplot(df, aes(x = country, y = life_satisfaction, fill = country)) + 
+    geom_boxplot() + 
+    labs(
+      title = "Boxplot of Life Satisfaction Across Countries",
+      x = "Country",
+      y = "Life Satisfaction"
+    ) + 
+    theme_minimal() + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+}
+make_plot2 <- function(df) {
+  ggplot(df, aes(x = Year)) +
+    geom_line(aes(y = life_satisfaction, color = "Life Satisfaction"), linewidth = 1) +
+    geom_line(aes(y = gdp_per_capita / 10000, color = "GDP per Capita"), linewidth = 1) +
+    facet_wrap(~ country) +
+    labs(
+      title = "Life Satisfaction and GDP per Capita Over Time",
+      x = "Year",
+      y = "Value (GDP scaled by 1/10000)",
+      color = "Variable"
+    ) +
+    scale_color_manual(values = c("Life Satisfaction" = "blue", "GDP per Capita" = "red")) +
+    theme_minimal()
 }
 
-make_plot2 <- function(data) {
-  ggplot(data, aes(x = category, y = value)) +
-    geom_boxplot()
-}
-
-fit_model <- function(data) {
-  lm(value ~ new_var + category, data = data)
+fit_model <- function(df) {
+  df %>%
+    group_by(country) %>%
+    nest() %>%
+    mutate(model = map(data, ~ lm(life_satisfaction ~ gdp_per_capita, data = .x)))
 }
--- _targets.R
+++ _targets.R
@@ -3,13 +3,18 @@ library(tarchetypes)
 
 source("R/functions.R")
 
-tar_option_set(packages = c("tidyverse", "readr", "ggplot2", "broom"))
+tar_option_set(packages = c("tidyverse", "readr", "ggplot2", "broom", "purrr"))
 
 list(
-  tar_target(raw_data, read_csv("data/your_data.csv")),
+  tar_target(raw_data, read_csv("data/gdp-vs-happiness.csv")),
   tar_target(clean_data, clean_and_wrangle(raw_data)),
   tar_target(plot1, make_plot1(clean_data)),
   tar_target(plot2, make_plot2(clean_data)),
-  tar_target(model_fit, fit_model(clean_data)),
-  tar_target(model_summary, broom::tidy(model_fit))
+  tar_target(model_fit, fit_model(clean_data)),  # returns nested df with models
+  tar_target(model_summary,
+             model_fit %>%
+               mutate(tidy_model = map(model, broom::tidy)) %>%
+               select(country, tidy_model) %>%
+               unnest(tidy_model)
+  )
 )
